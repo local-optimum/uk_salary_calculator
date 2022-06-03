8994e8c49403197095023fa9c54f37007c0e56d9
local-optimum <local_optimum+developer@protonmail.com>
2022-06-03 15:13:00 +0100

    remove unncessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:yaru/yaru.dart';
 import 'package:intl/intl.dart';
